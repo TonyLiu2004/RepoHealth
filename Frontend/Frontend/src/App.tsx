efabcaf303daf5e65bb9a5c78b87a416ceb2a793
Tony Liu <tony.liu404@gmail.com>
2026-04-21 15:59:51 -0400

    Initialized frontend

---
+++ Frontend/Frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <h1>RepoHealth</h1>
+      </section>
+
+      <section id="spacer"></section>
+
+      <section id="columns">
+        <div id="col1">
+          <h2>c1</h2>
+        </div>
+        <div id="col2">
+          <h2>c2</h2>
+        </div>
+        <div id="col3">
+          <h2>c3</h2>
+        </div>
+      </section>
+
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
